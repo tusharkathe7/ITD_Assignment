d18247aa5affabe92489086aa3565c182e0ba5c5
tusharkathe7 <tusharkathe7@gmail.com>
2025-03-01 13:49:56 +0530

    2203031080236(Assignment)

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { ForgotPasswordComponent } from './auth-user/forgot-password/forgot-password.component';
+import { User1Component } from './auth-user/user/user1/user1.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent,RegisterComponent,ForgotPasswordComponent,User1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Assignment1';
 }
